019329bd9100e43b8d9c02d3d4ec385737f64bdb
Chetan Gandhi <projectchetan07@gmail.com>
2017-07-03 21:05:04 +0530

    Updated readme
    - Code cleanup, removd fps and color change implementation, not required
    for now.
    - Updated readme and added preview image.

--- firstTriangle/main.cpp
+++ firstTriangle/main.cpp
@@ -1,50 +1,14 @@
-#include "buildParam.h"
-#include <array>
-#include <chrono>
-#include <iostream>
-
-#include "renderer.h"
 #include "vulkanWindow.h"
-#include "utils.h"
-
-constexpr double PI = 3.14159265358979323846;
-constexpr double CIRCLE_RAD = PI * 2;
-constexpr double CIRCLE_THIRD = CIRCLE_RAD / 3.0;
-constexpr double CIRCLE_THIRD_1 = 0;
-constexpr double CIRCLE_THIRD_2 = CIRCLE_THIRD;
-constexpr double CIRCLE_THIRD_3 = CIRCLE_THIRD * 2;
 
 int main()
 {
     VulkanWindow *window = new VulkanWindow(800, 600, "VulkanWindow", "Vulkan Window");
 
-    // float colorRotator = 0.0f;
-    // auto timer = std::chrono::steady_clock();
-    // auto lastTime = timer.now();
-    // uint64_t frameCounter = 0;
-    // uint64_t fps = 0;
-
     while(window->run())
     {
         // CPU Logic
 
-        // #if ENABLE_FPS
-
-        // ++frameCounter;
-
-        // if(lastTime + std::chrono::seconds(1) < timer.now())
-        // {
-        //     lastTime = timer.now();
-        //     fps = frameCounter;
-        //     frameCounter  = 0;
-        //     std::cout<<"----- FPS: "<<fps<<" -----"<<std::endl;
-        // }
-
-        // #endif // ENABLE_FPS
-
-        // colorRotator += 0.001;
-
-        // Begin rendering
+        // Render screen
 
         window->render();
     }
--- firstTriangle/readme.md
+++ firstTriangle/readme.md
@@ -1,17 +1,29 @@
-First Window
+First Triangle
 ============
+
+This base implementation show how to create a triangle with multi-color blending.
 
 #### How to compile
 
-* Open Visual Studio developer command prompt and use following command to compile the code.
+* Open Visual Studio developer command prompt and use following command to compile the code and shaders.
 
-```
-cl.exe /EHsc /D:UNICODE /DEBUG /I %VK_SDK_PATH%\Include *.cpp /link /LIBPATH:%VK_SDK_PATH%\lib user32.lib gdi32.lib kernel32.lib vulkan-1.lib
-```
+###### Compile Shader's
 
-* To compile shaders
 ```
 cd shaders
 glslangValidator.exe -V shader.vert
 glslangValidator.exe -V shader.frag
 ```
+
+###### Compile Code
+
+```
+cl.exe /EHsc /D:UNICODE /DEBUG /I %VK_SDK_PATH%\Include *.cpp /link /LIBPATH:%VK_SDK_PATH%\lib user32.lib gdi32.lib kernel32.lib vulkan-1.lib
+```
+###### Preview
+
+![firstTriangle][firstTriangle-image]
+
+<!-- Image declaration -->
+
+[firstTriangle-image]: ./preview/firstTriangle.png "First Triangle"
