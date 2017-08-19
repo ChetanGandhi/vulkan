ace908c760f14202cb3548ad8b5b827fc5e453b0
Chetan Gandhi <projectchetan07@gmail.com>
2017-08-19 20:13:21 +0530

    Fix source and destination pipeline stages flags in image pipeline barriers.

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -1400,27 +1400,39 @@ void Renderer::transitionImageLayout(VkImage image, VkFormat format, VkImageLayo
     imageMemoryBarrier.subresourceRange.baseArrayLayer = 0;
     imageMemoryBarrier.subresourceRange.layerCount = 1;
 
+    VkPipelineStageFlags sourceStageMask;
+    VkPipelineStageFlags destinationStageMask;
+
     if(oldImageLayout == VK_IMAGE_LAYOUT_UNDEFINED && newImageLayout == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL)
     {
         imageMemoryBarrier.srcAccessMask = VK_ACCESS_HOST_WRITE_BIT;
         imageMemoryBarrier.dstAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+
+        sourceStageMask = VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT;
+        destinationStageMask = VK_PIPELINE_STAGE_TRANSFER_BIT;
     }
     else if(oldImageLayout == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL && newImageLayout == VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL)
     {
         imageMemoryBarrier.srcAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
         imageMemoryBarrier.dstAccessMask = VK_ACCESS_SHADER_READ_BIT;
+
+        sourceStageMask = VK_PIPELINE_STAGE_TRANSFER_BIT;
+        destinationStageMask = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT;
     }
     else if(oldImageLayout == VK_IMAGE_LAYOUT_UNDEFINED && newImageLayout == VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL)
     {
         imageMemoryBarrier.srcAccessMask = 0;
         imageMemoryBarrier.dstAccessMask = VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_READ_BIT | VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;
+
+        sourceStageMask = VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT;
+        destinationStageMask = VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT;
     }
     else
     {
         assert(0 && "Unsupported layout transition!!!");
     }
 
-    vkCmdPipelineBarrier(commandBuffer, VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT, VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT, 0, 0, nullptr, 0, nullptr, 1, &imageMemoryBarrier);
+    vkCmdPipelineBarrier(commandBuffer, sourceStageMask, destinationStageMask, 0, 0, nullptr, 0, nullptr, 1, &imageMemoryBarrier);
 
     endOneTimeCommand(commandBuffer);
 }
