83883b292665b753a8fbfe77e40cc1f2fb1748bb
Chetan Gandhi <projectchetan07@gmail.com>
2019-04-13 17:11:27 +0530

    GLM Alignment requirements
    - Updated implementation to have GLM align data in uniform buffer.

--- src/platform.h
+++ src/platform.h
@@ -3,6 +3,12 @@
 #define GLM_ENABLE_EXPERIMENTAL
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
+
+// Vulkan expects the data in your structure to be aligned in memory in a specific way
+// We can use alignas(x) in structure before the variable deceleration
+// but we can tell GLM to force alignment by this way.
+#define GLM_FORCE_DEFAULT_ALIGNED_GENTYPES
+
 #define STB_IMAGE_IMPLEMENTATION
 #define TINYOBJLOADER_IMPLEMENTATION
 
