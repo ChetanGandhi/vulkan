d6c20f3cbb59881dd0f1e4e222b60d5b49920958
Chetan Gandhi <projectchetan07@gmail.com>
2019-11-02 14:23:23 +0530

    Remove redundant depth image transition
    - We don't need to explicitly transition the layout of the
      image to a depth attachment.
      We will take care of this in the render pass.

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -963,14 +963,6 @@ void Renderer::initDepthStencilImage()
         VK_IMAGE_ASPECT_DEPTH_BIT,
         1
     );
-
-    transitionImageLayout(
-        this->vkState->depthImage,
-        depthStencilFormat,
-        VK_IMAGE_LAYOUT_UNDEFINED,
-        VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL,
-        1
-    );
 }
 
 void Renderer::destoryDepthStencilImage()
@@ -1596,21 +1588,7 @@ void Renderer::transitionImageLayout(VkImage image, VkFormat format, VkImageLayo
     imageMemoryBarrier.srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
     imageMemoryBarrier.dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
     imageMemoryBarrier.image = image;
-
-    if(newImageLayout == VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL)
-    {
-        imageMemoryBarrier.subresourceRange.aspectMask = VK_IMAGE_ASPECT_DEPTH_BIT;
-
-        if(hasStencilComponent(format))
-        {
-            imageMemoryBarrier.subresourceRange.aspectMask |= VK_IMAGE_ASPECT_STENCIL_BIT;
-        }
-    }
-    else
-    {
-        imageMemoryBarrier.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
-    }
-
+    imageMemoryBarrier.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
     imageMemoryBarrier.subresourceRange.baseMipLevel = 0;
     imageMemoryBarrier.subresourceRange.levelCount = mipLevels;
     imageMemoryBarrier.subresourceRange.baseArrayLayer = 0;
@@ -1634,14 +1612,6 @@ void Renderer::transitionImageLayout(VkImage image, VkFormat format, VkImageLayo
 
         sourceStageMask = VK_PIPELINE_STAGE_TRANSFER_BIT;
         destinationStageMask = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT;
-    }
-    else if(oldImageLayout == VK_IMAGE_LAYOUT_UNDEFINED && newImageLayout == VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL)
-    {
-        imageMemoryBarrier.srcAccessMask = 0;
-        imageMemoryBarrier.dstAccessMask = VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_READ_BIT | VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;
-
-        sourceStageMask = VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT;
-        destinationStageMask = VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT;
     }
     else if(oldImageLayout == VK_IMAGE_LAYOUT_UNDEFINED && newImageLayout == VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL)
     {
